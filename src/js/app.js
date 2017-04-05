7d26a6bcb9c398586665631dc867957521e97f2c
Serhii <kso_132@mail.ru>
2017-04-05 12:44:42 +0300

    footer

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,17 @@
 //=include lib/svgxuse.min.js
 
 sayHello();
+
+$(document).ready(function(){
+
+	$(".menu-btn").on("click", function(){
+		var _this = $(this);
+		_this.toggleClass("menu-btn-is-active");
+		$(".menu-js").slideToggle(300);
+		//if(windowWidth <= 991 && windowWidth >= 768){
+		//	$(".js-hidden").toggleClass("is-hidden");
+		//};
+	});
+
+
+});
